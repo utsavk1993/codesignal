09a452a90a72d65fa260457f7d17019c31607d68
Utsav Kumar <utsavkumar.ca@gmail.com>
2023-07-09 23:46:00 -0700

    Create add.js

---
+++ intro/add.js
@@ -0,0 +1,5 @@
+/* Write a function that returns the sum of two numbers.
+ * Example: param1 = 1 and param2 = 2, the output should be solution(param1, param2) = 3
+ */
+
+const solution = (param1, param2) => param1 + param2;
